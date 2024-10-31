71ba542fc0d127bacb06effaf67bef34c7cbecca
claudiarojas2400 <claudiarojas2400@outlook.com>
2024-10-30 21:54:53 -0600

    citas listo

---
+++ BeautySalon/Models/Citas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeautySalon.Models
+{
+    public class Citas
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string NombreCompleto { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string CorreoElectronico { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Servicios { get; set; }
+
+        public DateOnly Fecha { get; set; }
+
+        [Required]
+        [Phone]
+        [StringLength(15)]
+        public string NumeroContacto { get; set; }
+    }
+}
