d4bd2902cb090ddfb1d314e910074785afa6933a
claudiarojas2400 <claudiarojas2400@outlook.com>
2024-11-11 22:07:09 -0600

    agrege JAVA

---
+++ BeautySalon/Controllers/SearchProducts.cs
@@ -0,0 +1,31 @@
+﻿using BeautySalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BeautySalon.Controllers
+{
+    public class SearchProductsController : Controller
+    {
+        private readonly BDContext _context;
+
+        public SearchProductsController(BDContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult SearchProducts(string searchTerm)
+        {
+            var products = _context.Producto
+                .Where(p => p.Nombre.Contains(searchTerm))
+                .Select(p => new
+                {
+                    p.Id,
+                    p.Nombre,
+                    p.PrecioUnitario
+                }) 
+                .ToList();
+            
+            return Json(products);
+        }
+    }
+}
